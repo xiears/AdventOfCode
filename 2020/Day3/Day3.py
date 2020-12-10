085c615c758f2cf8cd03ab930be0b062b68876ea
xiears <simon.hewitt@gmail.com>
2020-12-10 21:22:38 +0000

    Tweaks for Part 2

--- 2020/Day3/Day3.py
+++ 2020/Day3/Day3.py
@@ -5,17 +5,29 @@ def readInput(pathToInput):
     inputFile = open(fn)
     return inputFile.readlines()
 
-def countSlopeTree(terrain):
+def countSlopeTree(terrain, downInc, rightInc):
     x = 0
+    y = 0
+
     total = 0
-    for line in terrain:
-        if line[x] == "#":
+
+    while y < len(terrain):
+        if terrain[y][x] == "#":
             total = total + 1
-        x = (x + 3) % 31
+        x = (x + rightInc) % 31
+        y = y + downInc
 
     return total
 
 
 inputArray = readInput('Input/input')
 
-print (countSlopeTree(inputArray))
+print (countSlopeTree(inputArray, 1, 3))
+
+slope1 = countSlopeTree(inputArray, 1, 1)
+slope2 = countSlopeTree(inputArray, 1, 3)
+slope3 = countSlopeTree(inputArray, 1, 5)
+slope4 = countSlopeTree(inputArray, 1, 7)
+slope5 = countSlopeTree(inputArray, 2, 1)
+
+print(slope1*slope2*slope3*slope4*slope5)
