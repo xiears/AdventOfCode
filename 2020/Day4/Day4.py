1940f02e2feeaf467d75865d54521346dc808e71
xiears <simon.hewitt@gmail.com>
2020-12-14 12:19:45 +0000

    Finished Day 4

--- 2020/Day4/Day4.py
+++ 2020/Day4/Day4.py
@@ -10,48 +10,59 @@ def readInput(pathToInput):
 def checkPassport(passportString, terms):
     isValid = True
     for term in terms:
-        isValid = isValid & (term in passportString)
+        isValid = isValid and (term in passportString)
     return isValid
 
 def checkYear(year, low, high):
-    return (low <= year) & (year <= high)
+    return (low <= year) and (year <= high)
 
 def checkHeight(height):
     if height[-2:] == "cm":
         heightVal = int(height[:-2])
-        return (heightVal >= 150) & (heightVal <= 193)
+        return (heightVal >= 150) and (heightVal <= 193)
     elif height[-2:] == "in":
         heightVal = int(height[:-2])
-        return (heightVal >= 59) & (heightVal <= 76)
+        return (heightVal >= 59) and (heightVal <= 76)
     else:
         return False
 
 def checkHair(hair):
-    return re.match("#\\d{6}", hair)
+    regex = re.compile(r"#[\da-f]{6}\b")
+    return regex.match(hair)
 
 def checkEyes(eyes):
     return eyes in ["amb","blu","brn","gry","grn","hzl","oth"]
 
 def checkPid(pid):
-    return re.match("\\d{9}", pid)
+    regex = re.compile(r"\d{9}\b")
+    if len(pid) > 9:
+        print("idajoiajsfaoijiajfsaiaojiadjiosjdaoidjioajdsaoijfioajfioajfiasjfiajsidjaiodjaoisjd")
+    return regex.match(pid)
 
 def reallyCheckPassport(passportString):
     passportString = "{\'" + passportString.replace(":","\':\'").replace(" ", "\',\'").replace("\n","\',\'") + "\'}"
     details = ast.literal_eval(passportString)
 
-    return (checkYear(details["byr"], 1920, 2002) & checkYear(details["iyr"], 2010, 2020) & checkYear(details["eyr"], 2020, 2030) &
-    checkHeight(details["hgt"]) & checkHair(details["hcl"]) & checkEyes(details["ecl"]) & checkPid(details["pid"]))
+    return (checkYear(int(details["byr"]), 1920, 2002) and checkYear(int(details["iyr"]), 2010, 2020) and checkYear(int(details["eyr"]), 2020, 2030) and
+    checkHeight(details["hgt"]) and checkHair(details["hcl"]) and checkEyes(details["ecl"]) and checkPid(details["pid"]))
 
 
 requiredFields = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
-inputs = readInput('Input/allValid').split("\n\n")
+inputs = readInput('Input/input').split("\n\n")
+total = 0
 totalValid = 0
 totalReallyValid = 0
 
 for pp in inputs:
+    total += 1
     if checkPassport(pp, requiredFields):
         totalValid += 1
-    if reallyCheckPassport(pp):
-        totalReallyValid += 1
-    
+        if reallyCheckPassport(pp):
+            totalReallyValid += 1
+            print (totalReallyValid)
+            print(pp)
+            print ('*******************VALID**************************')
+
+print(total)
 print(totalValid)
+print(totalReallyValid)
